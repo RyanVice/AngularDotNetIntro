3141ce924574f4a8b4d2030561ca0b2e842e7656
Ryan Vice <ryan.vice@esosolutions.com>
2014-03-24 14:30:05 -0500

    Cart Part 4 - Add\Edit

--- src/AngularJsDotNetIntro/Scripts/_tests/tests.js
+++ src/AngularJsDotNetIntro/Scripts/_tests/tests.js
@@ -54,5 +54,119 @@ describe("When using the shopping cart", function () {
 
         scope.deleteItem(1);
         expect(scope.getTotal()).toBe(53.94);
-    }); 
+    });
+
+    it("should have an accurate total after an edit.", function () {
+        scope.items = [
+            {
+                id: 1,
+                name: "Blocks",
+                quantity: 2,
+                price: 20.00
+            },
+            {
+                id: 2,
+                name: "Nabi 2",
+                quantity: 1,
+                price: 150.00
+            },
+            {
+                id: 3,
+                name: "Tin Robot",
+                quantity: 4,
+                price: 5.00
+            }
+        ];
+
+        expect(scope.getTotal()).toBe(210);
+
+        var expectedName = "Blocks Changed";
+        var expectedQuantity = 5;
+        var expectedPrice = 10.00;
+        scope.item = {
+            id: 1,
+            name: expectedName,
+            quantity: expectedQuantity,
+            price: expectedPrice
+        };
+
+        scope.updateItem();
+
+        expect(scope.items[0].name).toBe(expectedName);
+        expect(scope.items[0].quantity).toBe(expectedQuantity);
+        expect(scope.items[0].price).toBe(expectedPrice);
+        expect(scope.getTotal()).toBe(220);
+    });
+
+    it("should have an accurate total after an add.", function () {
+        scope.items = [
+            {
+                id: 1,
+                name: "Blocks",
+                quantity: 2,
+                price: 20.00
+            },
+            {
+                id: 2,
+                name: "Nabi 2",
+                quantity: 1,
+                price: 150.00
+            },
+            {
+                id: 3,
+                name: "Tin Robot",
+                quantity: 4,
+                price: 5.00
+            }
+        ];
+
+        expect(scope.getTotal()).toBe(210);
+
+        var expectedName = "Lego";
+        var expectedQuantity = 2;
+        var expectedPrice = 10.00;
+        scope.item = {
+            name: expectedName,
+            quantity: expectedQuantity,
+            price: expectedPrice
+        };
+
+        scope.addItem();
+        var addedIndex = scope.items.length - 1;
+        expect(scope.items[addedIndex].name).toBe(expectedName);
+        expect(scope.items[addedIndex].quantity).toBe(expectedQuantity);
+        expect(scope.items[addedIndex].price).toBe(expectedPrice);
+        expect(scope.getTotal()).toBe(230);
+    });
+
+    it("should not be updating after loading.", function () {
+        expect(scope.updating).toBe(false);
+    });
+
+    it("should got from adding to editing and back when clicking edit and then update.", function () {
+        expect(scope.updating).toBe(false);
+
+        scope.items = [
+            {
+                id: 1,
+                name: "Blocks",
+                quantity: 2,
+                price: 20.00
+            }
+        ];
+
+        scope.editItem(0);
+
+        expect(scope.updating).toBe(true);
+
+        scope.item = {
+            name: "Blocks changed",
+            quantity: 2,
+            price: 20.00
+        };
+
+        scope.updateItem();
+
+        expect(scope.updating).toBe(false);
+    });
 });
--- src/AngularJsDotNetIntro/Scripts/app/app.js
+++ src/AngularJsDotNetIntro/Scripts/app/app.js
@@ -2,18 +2,23 @@
 
 angular.module('app', [])
     .controller('mainCtrl', function ($scope) {
+        $scope.updating = false;
+        $scope.item = {};
         $scope.items = [
             {
+                id: 1,
                 name: "Blocks",
                 quantity: 2,
                 price: 14.99
             },
             {
+                id: 2,
                 name: "Nabi 2",
                 quantity: 1,
                 price: 149.99
             },
             {
+                id: 3,
                 name: "Tin Robot",
                 quantity: 4,
                 price: 5.99
@@ -30,5 +35,35 @@ angular.module('app', [])
 
         $scope.deleteItem = function (index) {
             $scope.items.splice(index, 1);
+        };
+
+        $scope.editItem = function (index) {
+            $scope.updating = true;
+
+            $scope.item.id = $scope.items[index].id;
+            $scope.item.name = $scope.items[index].name;
+            $scope.item.price = $scope.items[index].price;
+            $scope.item.quantity = $scope.items[index].quantity;
+        };
+
+        $scope.updateItem = function () {
+            for (var i = 0; i < $scope.items.length; i++) {
+                if ($scope.items[i].id === $scope.item.id) {
+                    $scope.items[i] = $scope.item;
+                    break;
+                }
+            }
+
+            $scope.item = null;
+            $scope.updating = false;
+        };
+
+        $scope.addItem = function () {
+            $scope.items.push({
+                id: $scope.items.length + 1,
+                name: $scope.item.name,
+                price: $scope.item.price,
+                quantity: $scope.item.quantity
+            });
         };
     });
