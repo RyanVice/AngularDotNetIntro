e84cc0b24513bbe2ce4f0fb7c39fd0ecea58f01d
Ryan Vice <ryan.vice@esosolutions.com>
2014-03-29 06:35:50 -0500

    Got it working by updating my attempted revealing module pattern that wasn't very good to something that is working better.

---
+++ src/AngularJsDotNetIntro/Controllers/CartItemsController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace AngularSpaTest3.Controllers
+{
+    public class CartItemsController : ApiController
+    {
+        private static readonly List<CartItem> CartItems =
+            new List<CartItem>
+            {
+                new CartItem
+                {
+                    id = 1,
+                    name = "Block",
+                    quantity = 2,
+                    price = 14.99m
+                },
+                new CartItem
+                {
+                    id = 2,
+                    name = "Nabi 2",
+                    quantity = 1,
+                    price = 149.99m
+                },
+                new CartItem
+                {
+                    id = 3,
+                    name = "Tin Robot",
+                    quantity = 4,
+                    price = 5.99m
+                }
+            };
+
+        // GET api/values
+        public IEnumerable<CartItem> Get()
+        {
+            return CartItems;
+        }
+
+        // GET api/values/5
+        public CartItem Get(int id)
+        {
+            return GetItem(id);
+        }
+
+        
+        // POST api/values
+        public void Post(CartItem item)
+        {
+            CartItems.Add(item);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, CartItem item)
+        {
+            var itemToUpdate = GetItem(id);
+            item.id = id;
+            itemToUpdate = item;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            CartItem cartItem = GetItem(id);
+            CartItems.Remove(cartItem);
+        }
+
+        private static CartItem GetItem(int id)
+        {
+            return CartItems.Single(i => i.id == id);
+        }
+    }
+
+    public class CartItem
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int quantity { get; set; }
+    }
+}
--- src/AngularJsDotNetIntro/Scripts/app/app.js
+++ src/AngularJsDotNetIntro/Scripts/app/app.js
@@ -1,11 +1,14 @@
 ﻿'use strict';
 
-angular.module('app', [])
+angular.module('app', ['ngResource'])
     .controller('mainCtrl', function ($scope, cartSrv) {
         $scope.updating = false;
         $scope.item = {};
-        $scope.items = cartSrv.items;
-        $scope.getTotal = cartSrv.getTotal;
+
+        $scope.$on('cartUpdated', function () {
+            $scope.items = angular.copy(cartSrv.items);
+            $scope.getTotal = angular.copy(cartSrv.getTotal);
+        });
 
         $scope.deleteItem = function (index) {
             cartSrv.deleteItem(index);
@@ -30,95 +33,90 @@ angular.module('app', [])
         };
     })
     .controller('navBarCtrl', function ($scope, cartSrv) {
-        $scope.items = cartSrv.items;
-        $scope.numberInCart = cartSrv.items.length;
-
-        $scope.$watch('items', function (items) {
-            $scope.numberInCart = items.length;
-        }, true);
+        $scope.$on('cartUpdated', function () {
+            $scope.numberInCart = cartSrv.items.length;
+        });
     })
-    .factory('cartSrv', function () {
-        var nextItemId = 1;
-        var items = [
+    .factory('cartSrv', function ($resource, $rootScope) {
+        var cartItems = $resource(
+            '/api/CartItems/:id',
+            {},
             {
-                id: nextItemId++,
-                name: "Blocks",
-                quantity: 2,
-                price: 14.99
-            },
-            {
-                id: nextItemId++,
-                name: "Nabi 2",
-                quantity: 1,
-                price: 149.99
-            },
-            {
-                id: nextItemId++,
-                name: "Tin Robot",
-                quantity: 4,
-                price: 5.99
-            }
-        ];
-
-        var getTotal = function () {
-            var total = 0;
-            items.forEach(function (item) {
-                total = total + (item.price * item.quantity);
+                'get': { method: 'GET', isArray: true }
             });
-            return total;
-        };
 
-        var deleteItem = function (index) {
-            items.splice(index, 1);
-        };
+        var service = {
+            nextItemId: 1,
+            items: {},
 
-        var updateItem = function (item) {
-            for (var i = 0; i < items.length; i++) {
-                if (items[i].id === item.id) {
-                    items[i] = item;
-                    break;
+            getItems: function () {
+                this.items = cartItems.get();
+                this.items.$promise.then(function () {
+                    $rootScope.$broadcast('cartUpdated');
+                });
+                return promise;
+            },
+
+            getTotal: function () {
+                var total = 0;
+                this.items.forEach(function (item) {
+                    total = total + (item.price * item.quantity);
+                });
+                return total;
+            },
+
+            deleteItem: function (index) {
+                var me = this;
+                cartItems.delete({ id: me.items[index].id })
+                    .$promise.then(function () {
+                        me.getItems();
+                    });
+            },
+
+            updateItem: function (item) {
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].id === item.id) {
+                        items[i] = item;
+                        break;
+                    }
                 }
-            }
+            },
+
+            addItem: function (item) {
+                item.id = nextItemId;
+                if (items === null)
+                    items = [item];
+                else
+                    items.push(item);
+
+                return nextItemId++;
+            },
+
+            emtpyCart: function () {
+                items = null;
+            },
+
+            addItems: function (itemsToAdd) {
+                itemsToAdd.forEach(function (item) {
+                    addItem(item);
+                });
+            },
+
+            getItem: function (id) {
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].id === id)
+                        return items[i];
+                }
+                return null;
+            },
         };
 
-        var addItem = function (item) {
-            item.id = nextItemId;
-            if (items === null)
-                items = [item];
-            else
-                items.push(item);
+        service.items = cartItems.get();
+        service.items.$promise.then(function () {
+            $rootScope.$broadcast('cartUpdated');
+        });
 
-            return nextItemId++;
-        };
-
-        var emtpyCart = function () {
-            items = null;
-        };
-
-        var addItems = function (itemsToAdd) {
-            itemsToAdd.forEach(function (item) {
-                addItem(item);
-            });
-        };
-
-        var getItem = function (id) {
-            for (var i = 0; i < items.length; i++) {
-                if (items[i].id === id)
-                    return items[i];
-            }
-            return null;
-        };
-
-        return {
-            items: items,
-            deleteItem: deleteItem,
-            addItem: addItem,
-            updateItem: updateItem,
-            getTotal: getTotal,
-            emtpyCart: emtpyCart,
-            addItems: addItems,
-            getItem: getItem
-        }
+        return service;
     })
     .directive('vCurrency', function () {
         return {
